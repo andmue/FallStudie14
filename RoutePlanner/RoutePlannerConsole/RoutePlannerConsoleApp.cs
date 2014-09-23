9ed0ca61994bd556144df6dbf6d167375d55f450
mp66 <andreas_mueller@live.com>
2014-09-23 15:16:28 +0200

    Lab2: Task 1 & 2

--- RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -13,10 +13,10 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RoutePlanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
-            Console.ReadKey();
 
             var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312); 
             Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+            Console.WriteLine(wayPoint.ToString());
             Console.ReadKey();
         }
     }
--- RoutePlanner/RoutePlannerLib/WayPoint.cs
+++ RoutePlanner/RoutePlannerLib/WayPoint.cs
@@ -8,6 +8,7 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
     public class WayPoint
     {
+        private const int R = 6371;
         public string Name { get; set; } 
         public double Longitude { get; set; } 
         public double Latitude { get; set; }
@@ -17,6 +18,17 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
             Name = _name; 
             Latitude = _latitude; 
             Longitude = _longitude;
+        }
+
+        public override string ToString()
+        {
+            return "WayPoint: " + (Name ?? "") + " " + Latitude.ToString("F2") + "/" + Longitude.ToString("F2");
+        }
+
+        public double Distance(WayPoint target)
+        {
+            return R * Math.Acos(Math.Sin(Latitude)*Math.Sin(target.Latitude) +
+                   Math.Cos(Latitude)*Math.Cos(target.Latitude)*Math.Cos(Longitude - target.Longitude));
         }
     }
 }
