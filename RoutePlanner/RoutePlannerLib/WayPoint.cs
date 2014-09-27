ad0f46714e06a61ad13f9168529ad4787ce6ab2b
srhjg <sarah.geissberger@gmail.com>
2014-09-27 15:39:40 +0200

    Lab 2: Aufgabe 1b edited

--- RoutePlanner/RoutePlannerLib/WayPoint.cs
+++ RoutePlanner/RoutePlannerLib/WayPoint.cs
@@ -8,7 +8,7 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
     public class WayPoint
     {
-        private const int R = 6371;
+        private const int Radius = 6371;
         public string Name { get; set; } 
         public double Longitude { get; set; } 
         public double Latitude { get; set; }
@@ -25,10 +25,18 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
             return "WayPoint: " + (Name ?? string.Empty) + " " + Math.Round(Longitude,2) + " " + Math.Round(Latitude,2);
         }
 
+        /// <summary>
+        /// calculates the distance to targeted WayPoint
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="target"></param>
+        /// <returns>distance</returns>
         public double Distance(WayPoint target)
         {
-            return R * Math.Acos(Math.Sin(Latitude)*Math.Sin(target.Latitude) +
+            double distance = Radius * Math.Acos(Math.Sin(Latitude)*Math.Sin(target.Latitude) +
                    Math.Cos(Latitude)*Math.Cos(target.Latitude)*Math.Cos(Longitude - target.Longitude));
+
+            return distance;
         }
     }
 }
