05edd564c3eb78bd9a321f350aa3c6edbb67ec3b
srhjg <sarah.geissberger@gmail.com>
2014-09-28 20:29:33 +0200

    Lab 2: Aufgabe 2b (part)
    File IO, Collections, Indexers (wip)

---
+++ RoutePlanner/RoutePlannerLib/Cities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    class Cities
+    {
+        public int numberOfCities;
+
+        public int ReadCities(string filename)
+        {
+            numberOfCities = 0;
+            //read cities from file and safe in list
+            numberOfCities++;
+
+            return numberOfCities;
+        }
+    }
+}
