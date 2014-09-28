f14e54d4b9333c169015fe5f35474f51e7bcfb68
srhjg <sarah.geissberger@gmail.com>
2014-09-28 20:22:10 +0200

    Lab 2: Aufgabe 2a
    File IO, Collections, Indexer

---
+++ RoutePlanner/RoutePlannerLib/City.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    class City
+    {
+        public string Name {get; set;}
+        public string Country { get; set;}
+        public int Population { get; set; }
+        public WayPoint Location { get; set; }
+
+
+        public City(string _name, string _country, int _population, double _latitude, double _longitude)
+        {
+            Name = _name;
+            Country = _country;
+            Population = _population;
+            Location.Longitude = _longitude;
+            Location.Latitude = _latitude;
+        }
+
+    }
+}
