d968674da5fa934eccf3df166bc258ca6d269cf5
srhjg <sarah.geissberger@gmail.com>
2014-10-14 14:51:25 +0200

    Lab 5: Aufgabe 1b+c (part)
    added settings file and created RoutesFactory class

---
+++ RoutePlanner/RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            //TODO
+        }
+        
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            //TODO
+        }
+    }
+}
